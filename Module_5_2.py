ea37ee789c170ed7a8a1cdcc508e2425df787995
NastasiaAver <nastenka_napoli@rambler.ru>
2024-09-11 19:20:53 +0300

    Add files via upload

---
+++ Module_5_2.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, numbers_of_floors):
+        self.name = name
+        self.numbers_of_floors = numbers_of_floors
+
+
+    def __len__(self):
+        return self.numbers_of_floors
+
+    def __str__(self):
+        return f'Наименование: {self.name}, кол-во этажей: {self.numbers_of_floors}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
